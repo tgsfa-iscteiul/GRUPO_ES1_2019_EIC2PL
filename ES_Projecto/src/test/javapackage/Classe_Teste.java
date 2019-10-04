e74871058c76adef9f0bc4bb25c416a3aad81d02
tgsfa-iscteiul <tgsfa@iscte-iul.pt>
2019-10-04 15:15:47 +0100

    teste ES1

---
+++ ES_Projecto/src/test/javapackage/Classe_Teste.java
@@ -0,0 +1,25 @@
+package test.javapackage;
+
+public class Classe_Teste {
+
+
+
+
+
+	public int soma(int x, int y) {
+		return (x+y);
+	}
+
+
+
+
+	public static void main(String[] args) {
+
+		Classe_Teste teste = new Classe_Teste();
+		int x = teste.soma(7, 4);
+
+		System.out.println(x);
+
+	}
+
+}
